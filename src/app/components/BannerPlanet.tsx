913d402cb7ef874240cf70903bae5537afd52bb1
Hector Barazorda <ph.barazorda@gmail.com>
2023-06-23 12:38:47 -0500

    🐛 FIX: background banner planet

--- src/app/components/BannerPlanet.tsx
+++ src/app/components/BannerPlanet.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export const BannerPlanet = () => {
   return (
-    <div className="banner-planet ">
-      <h2 className=" text-title text-[49px] text-white text-4xl text-center bg-black/50">
+    <div className="banner-planet py-[300px]">
+      <h2 className=" text-title text-[49px] text-white text-4xl text-center bg-black/50 my-5">
         Una propuesta de cambio
       </h2>
-      <p className="text-center text-lg text-[19px] text-white w-[100%] md:w-[50%] bg-black/50">
+      <p className="text-center text-lg text-[19px] text-white mb-5 w-[100%] md:w-[40%] bg-black/50">
         Hemos de organizarnos, generar canales de comunicación para difundir
         nuevos estándares de justicia que trabajen para las personas, debemos
         integrar a un <strong>equipo de profesionales</strong> que ponga en
@@ -16,7 +16,7 @@ export const BannerPlanet = () => {
         con valores, y metas que permiten conformar un movimiento civil que
         impulse un cambio en la justicia.
       </p>
-      <button className="text-title text-[30px] bg-white  p-[9px] px-[20px] rounded-[63px]  ">
+      <button className="text-title text-[30px] bg-white  p-[9px] my-5 px-[20px] rounded-[63px]  ">
         SALIR DEL JUEGO
       </button>
     </div>
